5220173468219ed8571a6c8735d5604b8fcf6cd4
Kirill <kirillcnv@gmail.com>
2022-08-02 12:49:20 +0300

    add FirtTest

---
+++ src/test/java/com/demoqa/FirtsTest.java
@@ -0,0 +1,63 @@
+package com.demoqa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class FirtsTest {
+
+    @BeforeAll
+    static void configure() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.browserPosition = "0x0";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void autoTest(){
+        open("/automation-practice-form");
+        $("#firstName").setValue("Kirill");
+        $("#lastName").setValue("Chernyshov");
+        $("#userEmail").setValue("user@email.com");
+        $(byText("Male")).click();
+        $("#userNumber").setValue("7880852282");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").click();
+        $(".react-datepicker__month-select").selectOptionByValue("4");
+        $(".react-datepicker__year-select").selectOptionContainingText("1994");
+        $(".react-datepicker__year-select").click();
+        $(".react-datepicker__day--024").click();
+        $(byText("Sports")).click();
+        $("#subjectsInput").setValue("physical Culture");
+        $("#uploadPicture").uploadFile(new File("src/test/resources/1.jpg"));
+        $("#currentAddress").setValue("Russia, Mosсow");
+        //$(By.xpath("//h2[text()='" + form + "']//ancestor::div[@class='block-wrapper']")).scrollIntoView(false);
+        $(byText("Select State")).click();
+        $(byText("NCR")).click();
+        $(byText("Select City")).click();
+        $(byText("Delhi")).click();
+        $("#submit").click();
+
+
+        $(".modal-body").shouldHave(
+                text("Kirill"),
+                text("Chernyshov"),
+                text("user@email.com"),
+                text("Male"),
+                text("7880852282"),
+                text("24 May,1994"),
+                text("Sports"),
+                text("1.jpg"),
+                text("Russia, Mosсow"),
+                text("NCR Delhi") );
+    }
+}
